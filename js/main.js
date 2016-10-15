91b5d2e551ee3df720662034245b540a8af3ef92
Tyler Chen <chentylerl@gmail.com>
2016-10-15 12:36:12 -0400

    update

--- js/main.js
+++ js/main.js
@@ -12,8 +12,12 @@ function search(){
          });
 }
 
+locations=document.getElementById('locations')
 function getresults(data){
     console.log(data)
+    start="Room 402"
+    stop="Room 505"
+    locations.innerHTML= start+" to "+stop
 }
 
 function showmap(){
@@ -21,10 +25,18 @@ function showmap(){
 }
 
 
-floors = ['1F','2F','3F','4F']
-starting_floor=3 // (4th in floorlist)
-sel=document.getElementById('floor-selector')
-map=document.getElementById('map')
+
+
+function showsearch(){
+    $("#form-container").animate({bottom: "0"}, 700, false);
+}
+
+
+
+var floors = ['1F','2F','3F','4F']
+var starting_floor=3 // (4th in floorlist)
+var sel=document.getElementById('floor-selector')
+var map=document.getElementById('map')
 for(i=0; i<floors.length; i++){
     node=document.createElement("div")
     node.classList.add("level")
@@ -33,16 +45,40 @@ for(i=0; i<floors.length; i++){
 }
 
 sel.childNodes[starting_floor].classList.add("current")
-
+map.innerHTML=svg[starting_floor]
 
 
 levels=document.getElementsByClassName('level')
 for(i=0; i<levels.length; i++){
-    levels[i].addEventListener("click",changeLevel,false)
+    console.log(i)
+    levels[i].addEventListener("click",updatemap,false)
 }
 
-function changeLevel(){
+function updatemap(){
+    for(i=0; i<levels.length; i++){
+        if(levels[i] == this){
+            j=i
+            break  
+        } 
+    }
     document.getElementsByClassName("current")[0].classList.remove("current")
-    console.log(this.classList.add("current"))
-    map.src="img/"+this.innerHTML+"_path.svg"
+    this.classList.add("current")
+    console.log(map.innerHTML)
+    map.innerHTML=svg[j]
+}
+
+
+function renderpath(path){
+    for(i=0; i<path.length-1; i++){
+        console.log(path[i]+'-'+path[i+1])
+        e=document.getElementById(path[i]+'-'+path[i+1])
+        if(e!==null){
+            e.style.stroke="red"
+        } else {
+            e=document.getElementById(path[i+1]+'-'+path[i])
+            e.style.stroke="red"
+        }
+        document.getElementById(path[0]).style.fill="red"
+        document.getElementById(path[path.length-1]).style.fill="red"
+    }
 }
